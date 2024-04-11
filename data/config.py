0871f351bcdf1d65fe33a821def41ebdb49428e7
绝对值M <shenghong6560@gmail.com>
2024-04-12 05:40:12 +0800

    feat(更新配置信息): 更新配置信息
    1、删除邀请码，通过方法配置
    2、新增bsc和xterio查询接口

--- data/config.py
+++ data/config.py
@@ -6,12 +6,15 @@
 """
 import random
 
-BSC_RPC = "https://bsc-dataseed.bnbchain.org"
+BSC_RPC = "https://bsc-pokt.nodies.app"
 XTERIO_RPC = "https://xterio.alt.technology"
+BSC_EXPLORER_TX = "https://bscscan.com/tx/"
+XTERIO_EXPLORER_TX = "https://xterscan.io/tx/"
 
 # 这里需要设置跨链的金额，这是个随机数，最少为0.01，但是需要小于BSC资产金额
 rollup_amount = round(random.uniform(0.0101, 0.011), 5)
 
+invite_code = "bcfc319b5f0d68b8be4152c4016b2bcf"
+
 sleep_config = [1, 5]
 
-invite_code = "bcfc319b5f0d68b8be4152c4016b2bcf"  # 邀请码
